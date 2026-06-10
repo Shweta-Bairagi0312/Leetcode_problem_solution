d0a47403aa21e611ef253b401872cc526221094e
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-06-10 15:46:15 +0530

    Implement method to find missing multiples

---
+++ june10.3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+        Set<Integer>set = new HashSet<>();
+        for(int num : nums){
+            set.add(num);
+        }
+        for(int i = k;; i+= k){
+            if(!set.contains(i)) return i;
+        }
+    }
+}
