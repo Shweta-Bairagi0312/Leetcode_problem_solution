73e99a9f22e0b1a81a2b4b3a81945885a6e50206
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-06-21 15:32:15 +0530

    Implement main class to calculate range midpoint

---
+++ june21.10.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int mn = Integer.MAX_VALUE;
+            int mx = Integer.MIN_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                int x = sc.nextInt();
+                mn = Math.min(mn, x);
+                mx = Math.max(mx, x);
+            }
+
+            System.out.println((mx - mn + 1) / 2);
+        }
+
+        sc.close();
+    }
+}
