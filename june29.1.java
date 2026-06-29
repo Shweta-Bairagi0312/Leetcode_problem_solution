0c164c36a2440f389266430850c8a986b255996f
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-06-29 15:12:07 +0530

    Implement checkAlmostEquivalent method in Solution class

---
+++ june29.1.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean checkAlmostEquivalent(String word1, String word2) {
+       Map<Character,Integer>map = new HashMap<>();
+       for(int i = 0; i<word1.length(); i++){
+        map.put(word1.charAt(i),map.getOrDefault(word1.charAt(i),0)+1);
+        map.put(word2.charAt(i),map.getOrDefault(word2.charAt(i),0)-1);
+       } 
+
+       for(int freq: map.values()){
+        if(freq>3||freq<-3){
+            return false;
+        }
+       }
+       return true;
+    }
+}
