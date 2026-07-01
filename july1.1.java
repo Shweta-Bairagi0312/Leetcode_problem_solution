3017e35ee0e051df542ba1e711926b8c6584b6e5
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-07-01 13:02:22 +0530

    Implement checkDistances method in Solution class

---
+++ july1.1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean checkDistances(String s, int[] distance) {
+        int n = s.length();
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                if(s.charAt(i)==s.charAt(j)){
+                    int gap = j-i-1;
+                    int index = s.charAt(i)-'a';
+                    if(gap != distance[index]){
+                        return false;
+                    }
+                }
+            }
+         
+        }
+        return true;
+    }
+}
