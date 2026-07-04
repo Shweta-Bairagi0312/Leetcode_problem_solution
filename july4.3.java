92434fe9966755edad29766886ba69f3768ff4ed
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-07-04 09:31:17 +0530

    Add isAdjacentDiffAtMostTwo method in Solution class
    Implement method to check adjacent character differences

---
+++ july4.3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAdjacentDiffAtMostTwo(String s) {
+        for(int i = 0; i<s.length()-1; i++){
+            int a = s.charAt(i)-'0';
+            int b = s.charAt(i+1)-'0';
+            int diff = Math.abs(a-b);
+            if(diff>2){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
