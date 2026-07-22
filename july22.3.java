e1170611dc0d9693f0086be69924c5749fbd64aa
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-07-22 18:54:29 +0530

    Implement removeKdigits method in Solution class

---
+++ july22.3.java
@@ -0,0 +1,38 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        int n = num.length();
+        Stack<Character>st = new Stack<Character>();
+        int i = 0;
+        if(k==n){
+            return "0";
+        }
+        while(i<n){
+        while(k>0 && !st.isEmpty() && st.peek()>num.charAt(i) ){
+            st.pop();
+            k--;
+
+        }
+        st.push(num.charAt(i));
+        i++;
+    }
+
+    while(k>0){
+        st.pop();
+        k--;
+    }
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty())
+        sb.append(st.pop());
+        sb.reverse();
+       
+       while(sb.length()>1 && sb.charAt(0)=='0')
+        sb.deleteCharAt(0);
+        
+       
+
+        return sb.toString();
+
+
+    }
+}
