b6f6ba41509c0fc6b68ea64350c4ac6da4c5358b
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-07-26 06:07:10 +0530

    Implement sum calculation with input handling

---
+++ july26.5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long sum = n * (n + 1) / 2;
+
+        for (int i = 0; i < n - 1; i++) {
+            sum -= sc.nextLong();
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
