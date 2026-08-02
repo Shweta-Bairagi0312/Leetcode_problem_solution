7f6c4bd95b6b38d0147a933325461e7672388858
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-08-03 05:09:16 +0530

    Add isEven method to check if a number is even

---
+++ Aug3.4.java
@@ -0,0 +1,11 @@
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        int bitmask = 1;
+        if((n & bitmask)==0){
+            return true;
+        }
+        return false;
+        
+    }
+}
