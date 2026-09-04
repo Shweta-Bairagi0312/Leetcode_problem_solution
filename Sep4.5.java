2c58f043b4d3f5be9d08d219ee5c74e14421659c
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-09-04 06:12:27 +0530

    Implement firstPalindrome method in Solution class

---
+++ Sep4.5.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(String str: words){
+            if(isPalindrome(str)){
+                return str;
+            }
+        }
+        return "";
+
+    }
+    public boolean isPalindrome(String str){
+        int left = 0;
+        int right = str.length()-1;
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
