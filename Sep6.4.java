9685a10390c388aeb4f839a61bcd83e6dfae5cab
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-09-06 11:06:07 +0530

    Add findLHS method to Solution class
    Implement findLHS method to find the length of the longest harmonious subsequence.

---
+++ Sep6.4.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int maxlen = 0;
+        int j = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            if(nums[i]-nums[j]>1){
+                j++;
+            }
+            if(nums[i]-nums[j]==1){
+                maxlen = Math.max(maxlen,i-j+1);
+            }
+        }
+        return maxlen;
+
+    }
+}
