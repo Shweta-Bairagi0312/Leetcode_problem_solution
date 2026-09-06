ecd983b6c47fbc13f358b19c9a49a8bde51d7c65
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-09-06 10:23:12 +0530

    Implement lengthOfLongestSubstring method

---
+++ Sep6.3.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+       int maxlen = 0;
+       int j = 0;
+       Map<Character, Integer>map = new HashMap<>();
+       for(int i = 0; i<s.length(); i++){
+        char ch = s.charAt(i);
+        map.put(ch, map.getOrDefault(ch,0)+1);
+
+        while(map.get(ch)>1){
+            char leftchar = s.charAt(j);
+            map.put(leftchar,map.get(leftchar)-1);
+            j++;
+        }
+        maxlen = Math.max(maxlen,i-j+1);
+        
+        
+       
+
+       }
+       return maxlen;
+    }
+}
