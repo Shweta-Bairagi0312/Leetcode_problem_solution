a2301e87c9cff51515a43bc26726f72a4f12dbc5
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-09-07 04:37:39 +0530

    Add minSubArrayLen method to Solution class
    Implement a method to find the minimum length of a contiguous subarray of which the sum is at least the target.

---
+++ Sep7.2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) { 
+        int minlen = Integer.MAX_VALUE;
+        int j = 0;
+        int sum = 0;
+        for(int i = 0; i<nums.length; i++){
+            sum += nums[i];
+
+            while(sum>=target){
+                if(i-j+1<minlen){
+                    minlen = i-j+1;
+                }
+                sum -= nums[j];
+                j++;
+            }
+        }
+        return minlen;
+
+
+
+    }
+}
