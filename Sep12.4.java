1ea4ea461d8bc66cf1917fe8d6b364f572dd50dd
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-09-12 05:32:28 +0530

    Add sum calculation program
    Implement a program to calculate the sum of integers from 0 to n.

---
+++ Sep12.4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int sum = 0;
+
+        // code here
+        for(int i = 0; i<=n; i++){
+            sum += i;
+        } 
+        System.out.print(sum);
+      
+    }
+}
