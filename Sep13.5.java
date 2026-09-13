2882ab0d4c94120585b4d838d894b03f9125993c
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-09-13 06:55:17 +0530

    Implement myPow function for power calculation

---
+++ Sep13.5.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+
+        long power = n;
+
+        if (power == 0) {
+            return 1;
+        }
+
+        if (power < 0) {
+            return 1 / myPow(x, (int)(-power));
+        }
+
+        double half = myPow(x, (int)(power / 2));
+
+        if (power % 2 == 0) {
+            return half * half;
+        } else {
+            return half * half * x;
+        }
+    }
+}
