ac40c6929aec7100a1df5822356091d8a1c5da4d
Shweta Bairagi <shwetabairagi742@gmail.com>
2026-09-17 06:04:26 +0530

    Implement totalFruit method to calculate max fruits

---
+++ Sep17.2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int maxlen = 0;
+        int left = 0;
+        int right = 0;
+        Map<Integer,Integer>map = new HashMap<>();
+        while(right<fruits.length){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+             while(map.size()>=3){
+                map.put(fruits[left],map.getOrDefault(fruits[left],0)-1);
+                if(map.get(fruits[left])==0){
+                    map.remove(fruits[left]);
+                }
+                left++;
+
+            }
+            maxlen = Math.max(maxlen,right-left+1);
+            right++;
+        } 
+
+        return maxlen;  
+
+        
+    }
+}
